607cb9625d6c5a08042b23e7ffb7d91e4f56ecdd
fd <1136031926@qq.com>
2023-05-24 20:13:20 +0800

    second

---
+++ src/components/Collect/Collect.vue
@@ -0,0 +1,27 @@
+<template>
+    <el-container>  
+        shoucangjia 
+    </el-container>
+</template>
+
+<script>
+
+    
+    export default {
+        name: 'Collect',
+        components: {
+
+        },
+        data() {
+            return {
+
+            }
+        },
+        mounted() {
+           
+        },
+        methods: {
+
+        }
+    }
+</script>
